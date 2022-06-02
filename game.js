b90c1892ce7f324a7e6e384a2e2eeb53788f9352
MathildeEtoile <104795951+MathildeEtoile@users.noreply.github.com>
2022-06-02 11:39:31 +0200

    main js project

---
+++ game.js
@@ -0,0 +1,90 @@
+function computerPlay() {
+    let random = Math.floor(Math.random()*3)
+    let play = "Scissors"
+    if (random == 0) {
+        play = "Rock"
+    } else if (random == 1) {
+        play = "Paper"
+    }
+    return play
+}
+
+
+// console.log("The computer plays " + computerPlay() + ".")
+
+function playRound(playerSelection, computerSelection) {
+    let scoPlayer = 0;
+    let scoComputer = 0;
+    if (playerSelection.toLowerCase() == 'rock') {
+        const expr = computerSelection;
+        switch (expr) {
+            case 'Rock':
+                console.log('Draw!');
+                break;
+            case 'Paper':
+                scoComputer += 1
+                console.log('You lose! Paper beats Rock');
+                break;
+            case 'Scissors':
+                scoPlayer += 1;
+                console.log('You win! Rock beats Scissors');
+                break;
+            default:
+            console.log(`Sorry, there is something wrong.`);
+        }
+    } else if (playerSelection.toLowerCase() == 'paper') {
+        const expr = computerSelection;
+        switch (expr) {
+            case 'Rock':
+                scoPlayer += 1;
+                console.log('You win! Paper beats Rock');
+                break;
+            case 'Paper':
+                console.log('Draw!');
+                break;
+            case 'Scissors':
+                scoComputer += 1
+                console.log('You lose! Scissors beats Paper');
+                break;
+            default:
+            console.log(`Sorry, there is something wrong.`);
+        }
+    } else if (playerSelection.toLowerCase() == 'scissors') {
+        const expr = computerSelection;
+        switch (expr) {
+            case 'Rock':
+                scoComputer += 1
+                console.log('You lose! Rock beats Scissors');
+                break;
+            case 'Paper':
+                console.log('You win! Scissors beats Paper');
+                break;
+            case 'Scissors':
+                scoPlayer += 1;
+                console.log('Draw!');
+                break;
+            default:
+            console.log(`Sorry, there is something wrong.`);
+        }
+    } else {console.log(`Sorry, there is something wrong.`);}
+    return [scoPlayer, scoComputer]
+}
+
+// const playerSelection = 'Rock'
+// console.log("The player plays " + playerSelection)
+
+// console.log(playRound(playerSelection, computerPlay())[0])
+
+let scorePlayer = 0;
+let scoreComputer = 0;
+for (let i = 0; i<5; i++) {
+    let playerSelection = prompt("Rock, paper or scissors ?")
+    console.log("\nRound " + i)
+    let computerSelection = computerPlay()
+    let res = playRound(playerSelection, computerSelection)
+    scorePlayer += res[0]
+    scoreComputer += res[1]
+
+    console.log("Player has " + scorePlayer + " points ; Computer has " + scoreComputer + " points.\n")
+}
+console.log((scorePlayer > scoreComputer)? "\nYou win!" : (scoreComputer > scorePlayer)? "\nYou lose!" : "\nDraw!" )
